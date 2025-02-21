6e4deca2641a2e56a43d6d323ebbd7663a28efad
Kenneth Andrew <kennthandrew18@gmail.com>
2025-02-21 18:09:12 +0700

    Ft. React Router dom

--- src/projcet_frontend/src/main.jsx
+++ src/projcet_frontend/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router';
+import LoginPage from './Pages/LoginPage/LoginPage';
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>
+  },
+  
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route}></RouterProvider>
   </React.StrictMode>,
 );

2acde3656f2f42a4bfb914ef8c73cf4e29a0fc77
Ahingg <xaviero.yamin28@gmail.com>
2025-02-26 02:17:29 +0700

    feat -added login form modal, added modal context

--- src/projcet_frontend/src/components/Navbar.tsx
+++ src/projcet_frontend/src/components/Navbar.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import { X, Menu } from "lucide-react";
+import { X, Menu, PenLine } from "lucide-react";
+import { motion } from "framer-motion";
 import { AnimatedNavLink } from "./ui/animated-anchor";
+import { useModal } from "../contexts/modal-context";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const { setOpen } = useModal();
 
   return (
     <nav className="sticky bg-[#F9F7F7] text-black w-full z-50">
@@ -25,53 +26,16 @@ const Navbar = () => {
           </div>
 
           <div className="hidden md:flex items-center space-x-4">
-            <button className="bg-transparent hover:bg-opacity-90 px-4 py-2 rounded-md text-sm font-medium">
-              Log in
-            </button>
-          </div>
-
-          <div className="md:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-[#3F72AF]"
-            >
-              {isOpen ? <X size={24} /> : <Menu size={24} />}
-            </button>
-          </div>
-        </div>
-      </div>
-
-      {/* Mobile menu */}
-      <div className={`${isOpen ? "block" : "hidden"} md:hidden bg-[#112D4E]`}>
-        <div className="px-2 pt-2 pb-3 space-y-1">
-          <a
-            href="#"
-            className="block hover:bg-[#3F72AF] px-3 py-2 rounded-md text-base font-medium"
-          >
-            Find Work
-          </a>
-          <a
-            href="#"
-            className="block hover:bg-[#3F72AF] px-3 py-2 rounded-md text-base font-medium"
-          >
-            Post a Job
-          </a>
-          <a
-            href="#"
-            className="block hover:bg-[#3F72AF] px-3 py-2 rounded-md text-base font-medium"
-          >
-            Browse Freelancers
-          </a>
-          <a
-            href="#"
-            className="block hover:bg-[#3F72AF] px-3 py-2 rounded-md text-base font-medium"
-          >
-            How it Works
-          </a>
-          <div className="pt-4 space-y-2">
-            <button className="w-full bg-transparent hover:bg-opacity-90 px-4 py-2 rounded-md text-sm font-medium">
-              Sign In
-            </button>
+            <motion.div whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.98 }}>
+              <button
+                onClick={() => setOpen(true)}
+                className="w-full bg-transparent hover:bg-opacity-90 hover:border-2 px-4 py-2 rounded-2xl text-sm font-medium border-solid border border-black"
+              >
+                <div className="flex items-center justify-center text-center">
+                  Sign in <PenLine className="inline stroke-2 w-4" />
+                </div>
+              </button>
+            </motion.div>
           </div>
         </div>
       </div>

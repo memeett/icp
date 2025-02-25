08374337f860a31c17294f68632dd303b4893f52
vincentLagi <vincentkerja11@gmail.com>
2025-02-25 23:54:46 +0700

    FrontEnd FindJobPage Selesai

--- src/projcet_frontend/vite.config.ts
+++ src/projcet_frontend/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "https://127.0.0.1:4943",
+        target: "http://127.0.0.1:4943",
         changeOrigin: true,
       },
     },

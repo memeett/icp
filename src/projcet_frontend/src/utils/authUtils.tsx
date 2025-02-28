f0830a059dbbb72c89fe408fce8394b003aa6452
ixicept <piusphilip.purnama@gmail.com>
2025-02-28 23:19:56 +0700

    feat: add middleware and optimize navbar

--- src/projcet_frontend/src/utils/authUtils.tsx
+++ src/projcet_frontend/src/utils/authUtils.tsx
@@ -1,36 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
-import { getCookie, validateCookie, loginWithInternetIdentity, logout,fetchUserBySession } from "../controller/userController";
-
+import { getCookie, validateCookie, logout, fetchUserBySession } from "../controller/userController";
 
 export const authUtils = () => {
     const navigate = useNavigate();
     const cookie = getCookie("cookie");
     const session = localStorage.getItem("session");
-    useEffect(() => {
-        if (!cookie && !session) {
-            logout();
-        }
-        fetchUserBySession().then((user) => {
-            if (!user) {
-                logout();
-            }
-            
-        });
-        if (cookie == session && cookie && session) {
-            navigate('/');
-        }
-        else if (cookie && !session) {
-            console.log("Validating cookie");
-            validateCookie().then((val) => {
-                if (val) {
-                    navigate('/');
-                }
-            });
-        }
-        else if (cookie && (cookie !== session)) {
-            logout();
-        }
-    }, []);
 
-}
+    useEffect(() => {
+        const checkAuth = async () => {
+            if (!cookie && !session) {
+                navigate('/');
+                await logout();
+                return;
+            }
+
+            const user = await fetchUserBySession();
+            if (!user) {
+                navigate('/');
+                await logout();
+                return;
+            }
+            if (cookie && !session) {
+                console.log("Validating cookie");
+                const isValid = await validateCookie();
+                if (!isValid) {
+                    navigate('/');
+                    await logout();
+                    return;
+                }
+            } else if (cookie && cookie !== session) {
+                await logout();
+            }
+        };
+
+        checkAuth();
+    }, [cookie, session, navigate]);
+
+    return useMemo(() => ({ cookie, session }), [cookie, session]);
+};

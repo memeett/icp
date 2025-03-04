acfd5a274faccd6c2c4232af0557bf2a8fb1e222
Your Name <you@example.com>
2025-03-04 23:28:52 +0700

    edit job

--- src/projcet_frontend/src/components/modals/AuthenticationModal.tsx
+++ src/projcet_frontend/src/components/modals/AuthenticationModal.tsx
@@ -13,6 +13,7 @@ export function AuthenticationModal({ modalIndex }: { modalIndex?: number }) {
   const { open, setOpen, closeModal } = useModal();
   const { openNestedModal } = useNestedModal();
 
+  
   const handleOpenFaceVerification = () => {
     openNestedModal(<FaceVerificationModal parentIndex={modalIndex || 0} index={0} />);
   };
@@ -26,6 +27,7 @@ export function AuthenticationModal({ modalIndex }: { modalIndex?: number }) {
   };
 
   if (!open && modalIndex === undefined) return null;
+
 
   return (
     <div className="hidden md:flex flex-column items-center space-x-4">

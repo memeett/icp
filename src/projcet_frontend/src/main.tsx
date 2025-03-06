3f58be0104223772c8beb11d557e4e55b08d973c
Your Name <you@example.com>
2025-03-06 21:25:31 +0700

    manage job detal

--- src/projcet_frontend/src/main.tsx
+++ src/projcet_frontend/src/main.tsx
@@ -18,6 +18,7 @@ import RegisterFace from "./Pages/LoginPage/RegisterFacePage.tsx";
 import LoginFace from "./Pages/LoginPage/LoginFace.tsx";
 import BrowseFreelancerPage from "./Pages/BrowseFreelancerPage/BrowseFreelancerPage.tsx";
 import PublicProfile from "./Pages/profile/PublicProfile.tsx";
+import ManageJobDetailPage from "./Pages/ManageJobDetailPage/ManageJobDetailPage.tsx";
 
 
 const route = createBrowserRouter([
@@ -60,7 +61,11 @@ const route = createBrowserRouter([
   {
     path: "/face-recognition/login",
     element: <LoginFace />,
-  }
+  },
+  {
+    path: "/manage-jobs/:jobId",
+    element: <ManageJobDetailPage />,
+  },
 ]);
 
 const rootElement = document.getElementById("root");

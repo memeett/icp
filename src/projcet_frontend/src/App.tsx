3fde5985318cc133f8b63bbbf2ba8d52a44c56d0
Ahingg <xaviero.yamin28@gmail.com>
2025-08-18 00:52:38 +0700

    feat: added AI Advisor Bot interface and floating button. Optional: change border and size

--- src/projcet_frontend/src/App.tsx
+++ src/projcet_frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AppProviders } from './app/providers/AppProviders';
 import { AppRouter } from './app/router';
 import InboxPanel from './components/inbox/InboxPanel';
+import AIAdvisor from './components/chat/AIAdvisor';
 import './styles/style.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <AppRouter />
         <InboxPanel />
+        <AIAdvisor />
       </div>
     </AppProviders>
   );
